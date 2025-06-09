409a8dd90cb91c65bbeb970217dc02ad537aac5c
Ps-0123 <parsasaber0123@gmail.com>
2025-06-09 09:50:30 +0330

    Employee Login

--- src/Person/Employee.java
+++ src/Person/Employee.java
@@ -48,4 +48,58 @@ public class Employee extends Person {
     //    public void printInfo(){
         public String fullName = getFirstName() + " " + getLastName();
     //}
+
+    public void userMenu(){
+        int role = Integer.parseInt(getEmployeeCode().substring(0,1));
+        int range=1;
+        if(role == 3) {
+            System.out.println("1. Loan requests\n2. Delete customer account");
+            range = 2;
+        }
+        if (role == 2){
+            System.out.println("1. Banking requests");
+        }
+
+        if(role ==1) {
+            System.out.println("1. Banking Requests\n2. Block customer account\n3. Employee list");
+            range = 3;
+        }
+        System.out.println("0. Exit\nEnter your choice:");
+        int choice = scanner.nextInt();
+        if (choice<0 || choice>range) {
+            System.out.println("Invalid choice");
+            userMenu();
+
+        }else {
+            choice *= range;
+            switch (choice) {
+                case 0 ->{}
+                case 1 -> {
+                    // Assist Banking request
+                    handlingRequests();
+                }
+                case 2-> {
+                    // Teller Loan Request
+                    handlingRequests();
+                }
+                case 3 ->{
+                    // Manage Banking request
+                    handlingRequests();
+                }
+                case 4 ->{
+                    // Teller Deleter customer account
+
+                }
+                case 6 ->{
+                    //Manage Block customer
+                }
+                case 9 ->{
+                    //Employee list
+                }
+                default -> System.out.println("Invalid choice");
+            }
+        }
+
+
+    }
 }
